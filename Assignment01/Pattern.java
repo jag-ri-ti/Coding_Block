7d3b151518c18d6adf4861c28cb82d992e680aa2
Jagriti <misshal4me@gmail.com>
2024-09-06 02:27:14 +0530

    Pattern Assignment

--- Assignment01/Pattern.java
+++ Assignment01/Pattern.java
@@ -116,7 +116,7 @@ public class Pattern {
                 for (int i = 1; i <= n; i++) {
 
                     for (int k = 1; k <= n - i; k++) {
-                        System.out.print(" ");
+                        System.out.print("   ");
                     }
 
                     for (int j = 1; j <= i * 2 - 1; j++) {
