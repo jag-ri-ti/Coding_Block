2f26f46120c6f59026b5441289231014921ddc4f
Jagriti <misshal4me@gmail.com>
2024-09-06 02:33:38 +0530

    Pattern Assingment

--- Assignment01/Pattern.java
+++ Assignment01/Pattern.java
@@ -120,7 +120,7 @@ public class Pattern {
                     }
 
                     for (int j = 1; j <= i * 2 - 1; j++) {
-                        System.out.print("*");
+                        System.out.print(" * ");
                     }
 
                     System.out.println("");
