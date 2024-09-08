e62c2135803a04311811a16e85562f4c59599430
Jagriti <misshal4me@gmail.com>
2024-09-08 11:08:11 +0530

    OnOFF Triangle

---
+++ Lecture01/Patterns/Triangle_OnOff.java
@@ -0,0 +1,22 @@
+package Lecture01.Patterns;
+
+import java.util.*;
+
+public class Triangle_OnOff {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1" + " ");
+                } else {
+                    System.out.print("0" + " ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
